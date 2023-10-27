f57c11977ce2f1c26ca266d905c0478626de7bcd
serhan <serhankonukluk@gmail.com>
2023-10-28 00:03:17 +0300

    added profit calculation tests

--- utils/calculator_test.go
+++ utils/calculator_test.go
@@ -1,12 +1,18 @@
 package utils
 
-import "testing"
+import (
+	"testing"
+)
 
-type addTest struct {
+type calculateChange struct {
 	entryPrice, exitPrice, expected float64
 }
 
-var calculatePercentageChangeData = [5]addTest{
+type calculateProfit struct {
+	entryPrice, amount, exitPrice, expected float64
+}
+
+var calculateChangeData = [5]calculateChange{
 	{10, 20, 100},
 	{27.5, 102.1, 271.27},
 	{56, 22, -60.71},
@@ -14,25 +20,36 @@ var calculatePercentageChangeData = [5]addTest{
 	{1, 1, 0},
 }
 
+var calculateProfitData = [5]calculateProfit{
+	{1, 1, 2, 1},
+	{10, 20, 22.36, 247.2},
+	{220, 13, 160, -780},
+	{155, 50, 155, 0},
+	{155, 800, 150, -4000},
+}
+
 func TestCalculatePercentageChangeBetweenTwoPrices(t *testing.T) {
 
-	for _, test := range calculatePercentageChangeData {
+	for num, test := range calculateChangeData {
 		result := CalculatePercentageChangeBetweenTwoPrices(test.entryPrice, test.exitPrice)
 
 		if result != test.expected {
-			t.Errorf("CalculatePercentageChangeBetweenTwoPrices(%f, %f) FAILED! Expected: %v | Got: %v", test.entryPrice, test.exitPrice, test.expected, result)
+			t.Errorf("%v. CalculatePercentageChangeBetweenTwoPrices(%f, %f) FAILED! Expected: %v | Got: %v", num, test.entryPrice, test.exitPrice, test.expected, result)
 		} else {
-			t.Logf("CalculatePercentageChangeBetweenTwoPrices(%f, %f) PASSED! Expected: %v | Got: %v", test.entryPrice, test.exitPrice, test.expected, result)
+			t.Logf("%v. CalculatePercentageChangeBetweenTwoPrices(%f, %f) PASSED! Expected: %v | Got: %v", num, test.entryPrice, test.exitPrice, test.expected, result)
 		}
 	}
 }
 
 func TestCalculateProfitByPrice(t *testing.T) {
-	result := CalculateProfitByPrice(10, 20, 10)
 
-	if result != 100 {
-		t.Errorf("CalculateProfitByPrice(10, 20, 10) FAILED! Expected: %v | Got: %v", 100, result)
-	} else {
-		t.Logf("CalculateProfitByPrice(10, 20, 10) PASSED! Expected: %v | Got: %v", 100, result)
+	for num, test := range calculateProfitData {
+		result := CalculateProfitByPrice(test.entryPrice, test.exitPrice, uint(test.amount))
+
+		if result != test.expected {
+			t.Errorf("%v. CalculateProfitByPrice(%v, %v, %v) FAILED! Expected: %v | Got: %v", num, test.entryPrice, test.amount, test.exitPrice, test.expected, result)
+		} else {
+			t.Logf("%v. CalculateProfitByPrice(%v, %v, %v) PASSED! Expected: %v | Got: %v", num, test.entryPrice, test.amount, test.exitPrice, test.expected, result)
+		}
 	}
 }
