be2fa1cd089775fb903a57177e67d974ddfcd431
serhan_kk <serhankonukluk@ftechlabs.com>
2023-11-01 12:22:15 +0300

    TestFormatTargetPriceByTargetProfit added

--- utils/formatter_test.go
+++ utils/formatter_test.go
@@ -17,6 +17,10 @@ type formatPrice struct {
 	entryPrice, change, newPrice float64
 }
 
+type formatTargetPrice struct {
+	entryPrice, amount, profit, targetPrice float64
+}
+
 var formatChangeData = [5]formatChange{
 	{10, 20, 100},
 	{27.5, 102.1, 271.27},
@@ -39,6 +43,14 @@ var formatPriceData = [5]formatPrice{
 	{82, -8, 75.44},
 	{1, 1, 1.01},
 	{50.1, -1, 49.59},
+}
+
+var formatTargetPriceData = [5]formatTargetPrice{
+	{200, 10, 208, 220.8},
+	{8, 1, 20, 28},
+	{7.65, 150, 300, 9.65},
+	{30, 100, -150, 28.5},
+	{1, 1, 1, 2},
 }
 
 func TestFormatPercentageChangeBetweenTwoPrices(t *testing.T) {
@@ -92,6 +104,28 @@ func TestFormatPriceAfterPercentageChange(t *testing.T) {
 		result := FormatPriceAfterPercentageChange(test.entryPrice, test.change, test.newPrice, true)
 		// "Entry Price: %v \t Exit Price: %v \t %s: %%%v"
 		pattern := `^Entry Price: -?\d+(\.\d+)? \| Change: %-?\d+(\.\d+)? \| New Price: -?\d+(\.\d+)?$`
+		t.Logf("pattern: %v\n", pattern)
+		matched, err := regexp.MatchString(pattern, result)
+
+		if err != nil {
+			t.Log(err)
+		}
+		
+		if matched {
+			t.Logf("%v PASSED!", result)
+		} else {
+			t.Errorf("%v FAILED!", result)
+		}
+	}
+}
+
+func TestFormatTargetPriceByTargetProfit(t *testing.T) {
+
+	for _, test := range formatTargetPriceData {
+
+		result := FormatTargetPriceByTargetProfit(test.entryPrice, uint(test.amount), test.profit, test.targetPrice, true)
+		// "Entry Price: %v \t Exit Price: %v \t %s: %%%v"
+		pattern := `^Entry Price: -?\d+(\.\d+)? \| Amount: -?\d+(\.\d+)? \| Target Profit: -?\d+(\.\d+)? \| Target Price: -?\d+(\.\d+)?$`
 		t.Logf("pattern: %v\n", pattern)
 		matched, err := regexp.MatchString(pattern, result)
 
